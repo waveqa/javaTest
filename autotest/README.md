1d6d0b005504495e934ef330d2c1a28305ac430d
waveqa <alex95@meta.ua>
2020-01-07 23:52:08 +0200

    readme + work on remove reports

---
+++ autotest/README.md
@@ -0,0 +1,9 @@
+Run tests
+- mvn clean test 
+
+Generate a report using one of the following command:
+- mvn allure:serve
+
+Report will be generated into temp folder. Web server with results will start.
+
+- mvn allure:report
--- autotest/src/test/java/TestClass.java
+++ autotest/src/test/java/TestClass.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -20,6 +23,27 @@ public class TestClass {
     public void init(){
         System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
         open("https://pikabu.ru/");
+    }
+
+    @AfterClass
+    public static void afterExec(){
+        LOG.info("After class started");
+
+
+            boolean isWindows = System.getProperty("os.name")
+                    .toLowerCase().startsWith("windows");
+
+        ProcessBuilder builder = new ProcessBuilder();
+        if (isWindows) {
+            builder.command("cmd.exe", "mvn allure:report");
+        } else {
+            builder.command("mvn allure:report");
+        }
+        builder.directory(new File(System.getProperty("user.home")));
+        Process process = builder.start();
+
+        //mvn allure:report
+        LOG.info("After class finished");
     }
 
     @Rule
@@ -42,14 +66,14 @@ public class TestClass {
             saveScreenshot(((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES));
         }
     };
-
+/*
     @Test
     public void loginWithIncorrectCredentials(){
         mainPage.login(commonHelper.getRandomName(), commonHelper.getRandomPassword(6,8));
         mainPage.getVerifyRecaptchaButton().shouldBe(Condition.visible)
                 .pressEscape();
         mainPage.clickHidePostByIndex(0);
-    }
+    }*/
 
     @Test
     public void seeNewPosts(){
@@ -58,6 +82,11 @@ public class TestClass {
     }
 
     @Test
+    public void seetest(){
+        assert 1==1;
+    }
+
+   /* @Test
     public void createNewUser(){
         mainPage.clickSignUp();
         mainPage.fillUsername(commonHelper.getRandomName());
@@ -66,5 +95,5 @@ public class TestClass {
         mainPage.fillPassword(commonHelper.getRandomPassword(6,8));
         mainPage.clickCreateAccount();
         sleep(5000);
-    }
+    }*/
 }
