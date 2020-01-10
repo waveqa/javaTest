330aabe3e234b47fe7ce0abc161f6ba4b6483124
oleksii.koliienko <alex95@meta.ua>
2020-01-10 18:19:34 +0200

    working on config file

--- autotest/src/test/java/TestClass.java
+++ autotest/src/test/java/TestClass.java
@@ -3,41 +3,32 @@ import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import static com.codeborne.selenide.Selenide.*;
-
-@RunWith(JUnit4.class)
 public class TestClass {
     MainPage mainPage = new MainPage();
     CommonHelper commonHelper = new CommonHelper();
 
-/*
-    @Test
-    public void loginWithIncorrectCredentials(){
-        mainPage.login(commonHelper.getRandomName(), commonHelper.getRandomPassword(6,8));
-        mainPage.getVerifyRecaptchaButton().shouldBe(Condition.visible)
-                .pressEscape();
-        mainPage.clickHidePostByIndex(0);
-    }*/
+//    @Test
+//    public void loginWithIncorrectCredentials(){
+//        mainPage.login(commonHelper.getRandomName(), commonHelper.getRandomPassword(6,8));
+//        mainPage.getVerifyRecaptchaButton().shouldBe(Condition.visible)
+//                .pressEscape();
+//        mainPage.clickHidePostByIndex(0);
+//    }
 
     @Test
     public void seeNewPosts(){
         mainPage.clickNewPosts();
-        mainPage.getVerifyRecaptchaButton().shouldBe(Condition.visible);
+        assert 1==2;
+//        mainPage.getVerifyRecaptchaButton().shouldBe(Condition.visible);
     }
 
-    @Test
-    public void seetest(){
-        assert 1==1;
-    }
-/*
-    @Test
-    public void createNewUser(){
-        mainPage.clickSignUp();
-        mainPage.fillUsername(commonHelper.getRandomName());
-        mainPage.fillPhoneNumber(commonHelper.getRandomPhoneNumber());
-        mainPage.fillEmail(commonHelper.getRandomEmail("test123"));
-        mainPage.fillPassword(commonHelper.getRandomPassword(6,8));
-        mainPage.clickCreateAccount();
-        sleep(5000);
-    }*/
+//    @Test
+//    public void createNewUser(){
+//        mainPage.clickSignUp();
+//        mainPage.fillUsername(commonHelper.getRandomName());
+//        mainPage.fillPhoneNumber(commonHelper.getRandomPhoneNumber());
+//        mainPage.fillEmail(commonHelper.getRandomEmail("test123"));
+//        mainPage.fillPassword(commonHelper.getRandomPassword(6,8));
+//        mainPage.clickCreateAccount();
+//    }
 }
--- autotest/src/test/java/TestConfig.java
+++ autotest/src/test/java/TestConfig.java
@@ -1,3 +1,5 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import io.qameta.allure.Attachment;
 import org.junit.*;
@@ -8,10 +10,15 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.junit.runners.Suite.SuiteClasses;
 
+import java.io.File;
 import java.io.IOException;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -20,17 +27,29 @@ import static com.codeborne.selenide.Selenide.open;
 @SuiteClasses({TestClass.class})
 public class TestConfig {
     private static final Logger LOG = LoggerFactory.getLogger(TestConfig.class);
-
+    private static ChromeDriverService service;
+    private WebDriver driver;
+// todo 1) structure of the project, file with data for precondition
     @BeforeClass
-    public static void init(){
+    public static void setUp(){
         LOG.info("Before class started");
+//        service = new ChromeDriverService.Builder()
+//                .usingDriverExecutable(new File("./chromedriver.exe"))
+//                .usingAnyFreePort()
+//                .build();
+        Configuration.startMaximized = true;
+        Configuration.timeout = 10000;
         System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+//        System.setProperty("chromeoptions.args", "--start-maximized");
+
+        Configuration.browser = "chrome";
         open("https://pikabu.ru/");
     }
 
-    @After
-    public void afterExec(){
+    @AfterClass
+    public static void afterExec(){
         LOG.info("After class started");
+//        service.stop();
         boolean isWindows = System.getProperty("os.name")
                 .toLowerCase().startsWith("windows");
 
@@ -59,9 +78,15 @@ public class TestConfig {
     }
 
     @Rule
+    public void printInConsole(){
+        LOG.info("Rule is executed in config");
+    }
+
+    @Rule
     public TestWatcher watchman = new TestWatcher() {
         @Override
         protected void failed(Throwable e, Description description) {
+            LOG.info("failed");
             screenshot();
         }
 
